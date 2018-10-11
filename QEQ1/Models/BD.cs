b1c6bfb7b7133225b444a11cb367392969c907e7
MARTIN FACUNDO CEVA ALVAREZ <43993081@ort>
2018-10-11 11:17:06 -0300

    LoginHomeIncompletos

---
+++ QEQ1/Models/BD.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.SqlClient;
+
+namespace QEQ1.Models
+{
+    public class BD
+    {
+        public static string connectionString = "Server=10.128.8.16.;Database=QEQB01;Trusted_connection=true;";
+        private static SqlConnection conectar()
+        {
+            SqlConnection sqlConectar = new SqlConnection(connectionString);
+            sqlConectar.Open();
+            return sqlConectar;
+        }
+        private static void desconectar(SqlConnection desconexion)
+        {
+            desconexion.Close();
+        }
+    }
+}
---
+++ QEQ1/Models/Usuario.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+
+namespace QEQ1.Models
+{
+    public class Usuario
+    {
+        private int _IDUsuario;
+
+        public int IDUsuario
+        {
+            get { return _IDUsuario; }
+            set { _IDUsuario = value; }
+        }
+
+        private string _Email;
+        [Required(ErrorMessage = "Ingresa un Email valido")]
+        public string Email
+        {
+            get { return _Email; }
+            set { _Email = value; }
+
+        }
+
+        private string _Contraseña;
+        [Required(ErrorMessage = "Contraseña incorrecta")]
+        [StringLength(18, MinimumLength = 8)]
+        public string Contraseña
+        {
+            get { return _Contraseña; }
+            set { _Contraseña = value; }
+        }
+        private string _NomUsuario;
+
+        public string NomUsuario
+        {
+            get { return _NomUsuario; }
+            set { _NomUsuario = value; }
+        }
+        private string _Rol;
+
+        public string Rol
+        {
+            get { return _Rol; }
+            set { _Rol = value; }
+        }
+
+        private int _Puntaje;
+
+        public int Puntaje
+        {
+            get { return _Puntaje; }
+            set { _Puntaje = value; }
+        }
+
+
+
+
+    }
+}
